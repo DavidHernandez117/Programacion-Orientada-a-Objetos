cf1002fb38b601cf340dbcb30c57967783c496f4
DavidHernandez117 <dh576776@gmail.com>
2025-02-15 23:19:23 -0600

    Add files via upload

---
+++ Practica/backend/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'productos'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Error conectando a MySQL:', err);
+        return;
+    }
+    console.log('Conectado a MySQL');
+});
+
+app.get('/productos', (req, res) => {
+    db.query('SELECT * FROM products', (err, results) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en http://localhost:3000');
+});
